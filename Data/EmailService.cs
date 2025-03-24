3420c3cb00a1907c0ecc6cecde5828d569f586f2
Kevin Natalini <89156137+KevinNata@users.noreply.github.com>
2025-03-24 17:58:16 +0100

    Elimino variables de entorno
    Modifico email service para obtener el mail y la contraseña desde el appsettings.

--- Data/EmailService.cs
+++ Data/EmailService.cs
@@ -1,12 +1,23 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
 
 public class EmailService
 {
     private readonly string _smtpServer = "smtp.gmail.com";
     private readonly int _smtpPort = 587; // Puerto para TLS
-    private readonly string _emailFrom = Environment.GetEnvironmentVariable("SMTP_EMAIL");
-    private readonly string _emailPassword = Environment.GetEnvironmentVariable("SMTP_PASSWORD");
+    private string _emailFrom => GetConfiguration()["SMTP:EmailFrom"];
+    private string _emailPassword => GetConfiguration()["SMTP:EmailPassword"];
+
+    private static IConfiguration GetConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return builder;
+    }
 
     public async Task EnviarCorreoAsync(string destinatario, string asunto, string mensaje)
     {
